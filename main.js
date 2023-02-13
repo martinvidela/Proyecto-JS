8f8a6e9b1618ed64c9338abd5dec363f9628f608
martinvidela <videla116@gmail.com>
2023-02-13 18:03:38 -0300

    calculado

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
-/* let nombreIngreso = prompt("Ingrese su SteamID");
+ let nombreIngreso = prompt("Ingrese su SteamID");
 
 while (nombreIngreso == "" || nombreIngreso == null) {
     nombreIngreso = prompt("Coloque un nombre valido")
 }
 
-alert("Bienvenido " + nombreIngreso); */
+alert("Bienvenido " + nombreIngreso); 
+
+function multiplicar(){
+    let num1 = document.getElementById("num1").value;
+    let multiplicacion = num1 * 1.75
+    alert("El precio final es: " + multiplicacion)
+}
