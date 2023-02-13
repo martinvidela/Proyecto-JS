6c7649b01de58ef93ec1570444fd5be8fd14e698
martinvidela <videla116@gmail.com>
2023-02-13 17:02:10 -0300

    Javascript

---
+++ main.js
@@ -0,0 +1,7 @@
+/* let nombreIngreso = prompt("Ingrese su SteamID");
+
+while (nombreIngreso == "" || nombreIngreso == null) {
+    nombreIngreso = prompt("Coloque un nombre valido")
+}
+
+alert("Bienvenido " + nombreIngreso); */
