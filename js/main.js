9460aeaf0ff3de09e4d0cac71fe5950bbe4bdaff
martinvidela <videla116@gmail.com>
2023-02-28 16:03:40 -0300

    prueba

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let nombreIngreso = prompt("Ingrese su nombre");
+
+while (nombreIngreso == "" || nombreIngreso == null) {
+    nombreIngreso = prompt("Coloque un nombre valido")
+}
+
+alert("Bienvenido " + nombreIngreso); 
+
+
+let ala = menosaaa
