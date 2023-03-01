607e5ebcc2fb4d2d71a7eefc0e5f72862eec1464
martinvidela <videla116@gmail.com>
2023-03-01 15:47:46 -0300

    buclesyobjetos

--- js/main.js
+++ js/main.js
@@ -1,10 +1,36 @@
-let nombreIngreso = prompt("Ingrese su nombre");
 
-while (nombreIngreso == "" || nombreIngreso == null) {
-    nombreIngreso = prompt("Coloque un nombre valido")
+class Persona{
+    nombre;
+    apellido;
+    email;
+
+    constructor() {}
+    setNombre(nombre){
+        this.nombre = nombre;
+    }
+    setApellido(apellido){
+        this.apellido = apellido;
+    }
+  
+    mostrardatos(){
+        console.log(this.nombre, this.apellido, this.email)
+        alert("Nos alegramos de tenerte otra vez, " + this.nombre + " " + this.apellido + "!")
+    }
+
 }
 
-alert("Bienvenido " + nombreIngreso); 
+const persona1 = new Persona();
 
 
-let ala = menosaaa
+persona1.nombre = prompt("Ingresa tu nombre!")
+persona1.apellido = prompt("Ingresa tu apellido!")
+
+while (persona1.nombre == "" || persona1.nombre == null) {
+    persona1.nombre = prompt("Coloque un nombre valido")
+}
+while (persona1.apellido == "" || persona1.apellido == null) {
+    persona1.apellido = prompt("Coloque un apellido valido")
+}
+
+persona1.mostrardatos()
+
